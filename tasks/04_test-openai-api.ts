a4bfdc8031341d4911e6b5693ef3e69f32d005bd
Adam Bukowski <bukowski.adam@gmail.com>
2023-10-26 18:48:01 +0200

    feat: add testing openai api

---
+++ tasks/04_test-openai-api.ts
@@ -0,0 +1,22 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const response = await openai.chat.completions.create({
+  model: "gpt-3.5-turbo",
+  messages: [
+    {
+      role: "user",
+      content: "hello",
+    },
+  ],
+  temperature: 1,
+  max_tokens: 256,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+});
+
+console.log(JSON.stringify(response, null, 2));
